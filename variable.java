064438abf6773dac58e0ffc2b30dfea1c64cd4e5
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-01-20 23:47:28 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class variable {
+    public static void main(String[] args) {
+        int x; //declaration
+        x=6; //initialization
+        System.out.println(x);
+        x=7;
+        System.out.println(x);
+        x=x+6;
+        System.out.println(x);
+        x=x-20;
+        System.out.println(x);
+
+    }
+}

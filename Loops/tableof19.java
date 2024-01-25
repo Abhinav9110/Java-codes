5dff6a2522905d85c95ae7c4e5ddeef718a0f6ff
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-01-25 09:35:03 +0530

    Add files via upload

---
+++ Loops/tableof19.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class tableof19 {
+    public static void main(String[] args) {
+        System.out.println("Even number between 1 to 100");
+        for (int i = 19; i <= 190; i++) {
+            if (i % 19 == 0) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}

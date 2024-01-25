3a181f1ba1c026c08a2675033b169eb1c7f762c6
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-01-25 09:33:08 +0530

    Add files via upload

---
+++ Loops/print1to100.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class print1to100 {
+    public static void main(String[] args) {
+        System.out.println("Printing from 1 to 100");
+        for(int i=1;i<100;i++){
+            System.out.println(i+" ");
+        }
+    }
+}

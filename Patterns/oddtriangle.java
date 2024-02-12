e954d8038b4b2da5c4b12816a743046f261b9cfc
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-12 21:20:19 +0530

    Add files via upload

---
+++ Patterns/oddtriangle.java
@@ -0,0 +1,17 @@
+package specialpattern;
+
+import java.util.Scanner;
+
+public class oddtriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(2*j-1 +" ");
+            }
+            System.out.println();
+        }
+    }
+}
